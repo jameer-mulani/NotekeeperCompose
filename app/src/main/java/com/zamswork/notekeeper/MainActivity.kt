a8b538f533ca89c46b323a3070317b873f261cff
jameer <jameer.a.mulani@gmail.com>
2024-02-14 21:25:32 +0530

    Changed theme and color scheme.

--- app/src/main/java/com/zamswork/notekeeper/MainActivity.kt
+++ app/src/main/java/com/zamswork/notekeeper/MainActivity.kt
@@ -3,12 +3,24 @@ package com.zamswork.notekeeper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.zamswork.notekeeper.ui.theme.NoteKeeperTheme
 
@@ -22,6 +34,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    
                 }
             }
         }
